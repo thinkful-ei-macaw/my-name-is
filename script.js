15a07505d17b2e5c6ac757f7ec584753ca191aa4
Kamela Williamson <kami63116@yahoo.com>
2020-02-10 14:38:06 -0700

    throw error with message

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict'
 
 function getYearOfBirth(age) {
+    if (age < 1)
+    throw new Error("Age can not be negative");
     return 2020 - age;
 
 }
@@ -10,4 +12,11 @@ function createGreeting(name, age) {
     return `Hi My ${name} is and I'm ${age} years old and I was born in ${YearOfBirth}`;
 
 
+}
+
+try {
+    const greeting1 = createGreeting(Kam, 35); 
+
+} catch(e) {
+    console.error(e.message);
 }
