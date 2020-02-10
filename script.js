7a4ed81f17e1c22fa921f88d0770b66be957ecd3
Kamela Williamson <kami63116@yahoo.com>
2020-02-10 14:17:51 -0700

    returned value

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 'use strict'
 
 function createGreeting() {
+    return `Hi My name is Kamela and I'm 35 years old`;
 
-
-
-console log('Hi My name is Kamela and I'm 35 years old);
 
 }
-createGreeting();
