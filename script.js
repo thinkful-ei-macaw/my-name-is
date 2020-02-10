5334b5fb45fa3ce4e91205c6e3d3b4b7c792e1c3
Kamela Williamson <kami63116@yahoo.com>
2020-02-10 14:52:27 -0700

    final solution

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 'use strict'
 
 function getYearOfBirth(age) {
-    if (age < 1)
-    throw new Error("Age can not be negative");
+    if (age < 1) {
+    throw new Error("Age can not be negative")};
     return 2020 - age;
 
 }
 
 function createGreeting(name, age) {
-    const yearOfBirth 2020 - age;
+    if (name === undefined || age === undefined) {
+    throw new Error("Arguments not valid")};
+
+    if (typeof age === NaN) {
+        throw new Error("Age must be a number")};
+    }
+
     return `Hi My ${name} is and I'm ${age} years old and I was born in ${YearOfBirth}`;
 
-
+    
 }
 
 try {
