2e1becec45862eb364700bcd5679ec98413a1fa7
Kamela Williamson <kami63116@yahoo.com>
2020-02-10 14:29:32 -0700

    moved year of birth calculation into new function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 'use strict'
 
+function getYearOfBirth(age) {
+    return 2020 - age;
+
+}
+
 function createGreeting(name, age) {
     const yearOfBirth 2020 - age;
-    return `Hi My ${name} is and I'm ${age} years old and I was born in ${yearOfBirth}`;
+    return `Hi My ${name} is and I'm ${age} years old and I was born in ${YearOfBirth}`;
 
 
 }
